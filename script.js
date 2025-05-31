8a61aa8446a72f3b30a9b05397b8ed94541e26ba
Jpapirs <joao.pedro.apsilva@gmail.com>
2025-05-31 20:46:44 -0300

    Added JavaScript

---
+++ script.js
@@ -0,0 +1,16 @@
+const section = document.querySelectorAll(".js-scroll");
+
+const AlturaWindow = window.innerHeight * 0.6;
+
+function aparecer() {
+  section.forEach((item) => {
+    const sectionTop = item.getBoundingClientRect().top - AlturaWindow;
+    if (sectionTop < 0) {
+      item.classList.add("ativo");
+    } else {
+      item.classList.remove("ativo");
+    }
+  });
+}
+
+window.addEventListener("scroll", aparecer);
