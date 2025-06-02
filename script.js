6ebceacd327480447701d03f14aafa95675274be
Jpapirs <joao.pedro.apsilva@gmail.com>
2025-06-02 18:53:10 -0300

    Fixed button

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const section = document.querySelectorAll(".js-scroll");
 
 const AlturaWindow = window.innerHeight * 0.6;
-
-function aparecer() {
-  section.forEach((item) => {
-    const sectionTop = item.getBoundingClientRect().top - AlturaWindow;
-    if (sectionTop < 0) {
-      item.classList.add("ativo");
-    } else {
-      item.classList.remove("ativo");
-    }
-  });
+if (section.length) {
+  function aparecer() {
+    section.forEach((item) => {
+      const sectionTop = item.getBoundingClientRect().top - AlturaWindow;
+      if (sectionTop < 0) {
+        item.classList.add("ativo");
+      } else {
+        item.classList.remove("ativo");
+      }
+    });
+  }
 }
 
 window.addEventListener("scroll", aparecer);
